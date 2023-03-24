a9e462a5a43a4f7b00c802f44ad9af83764c6b5a
Mark Ryan <99985185+MarkRRyan@users.noreply.github.com>
2023-03-24 16:24:13 -0700

    playing with templates

--- hello.py
+++ hello.py
@@ -1,18 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This is the main page <h1>HELLO</h1>"
+    return render_template('index.html')
 
-@app.route("/<name>")
-def user(name):
-    return f'hello {name}'
-
-@app.route('/admin')
-def admin():
-    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run()
